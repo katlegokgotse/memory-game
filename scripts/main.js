b035456265c4467c3ba0d8ff2cd3135b8ffb4235
Katlego Kgotse <katlegokgotse88@gmail.com>
2025-04-11 23:17:15 +0200

    setting up the card

---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+export const container = document.getElementsByClassName("container")
---
+++ scripts/src/components/card.js
@@ -0,0 +1,4 @@
+import { container } from "../../main";
+
+export const card = document.createElement('p')
+container.appendChild(card)
